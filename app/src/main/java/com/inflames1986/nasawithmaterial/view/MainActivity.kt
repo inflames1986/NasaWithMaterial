63d4025ef1f56589d27d76dac24c057c04d7a76b
TMichael <js5859168@gmail.com>
2022-06-01 10:54:59 +0300

    Добавление ресурсов и стилей, приложение запускается, продолжаю настраивать и выполнять ДЗ 1

--- app/src/main/java/com/inflames1986/nasawithmaterial/view/MainActivity.kt
+++ app/src/main/java/com/inflames1986/nasawithmaterial/view/MainActivity.kt
@@ -3,10 +3,16 @@ package com.inflames1986.nasawithmaterial.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.inflames1986.nasawithmaterial.R
+import com.inflames1986.nasawithmaterial.view.picture.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.MyGreenTheme)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState==null){
+            supportFragmentManager.beginTransaction().replace(R.id.container,
+                PictureOfTheDayFragment.newInstance()).commit()
+        }
     }
 }
