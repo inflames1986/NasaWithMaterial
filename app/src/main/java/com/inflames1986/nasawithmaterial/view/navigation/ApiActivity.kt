8f0bda1631e0f04aa8010784bbca55887a58a457
TMichael <js5859168@gmail.com>
2022-06-03 15:14:17 +0300

    Добавил фрагменты и лэйауты земля, марс, солн. система 2,ViewPagerAdapter Api Activity,

---
+++ app/src/main/java/com/inflames1986/nasawithmaterial/view/navigation/ApiActivity.kt
@@ -0,0 +1,29 @@
+package com.inflames1986.nasawithmaterial.view.navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.inflames1986.nasawithmaterial.databinding.ActivityApiBinding
+
+class ApiActivity : AppCompatActivity() {
+    lateinit var binding: ActivityApiBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityApiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        binding.viewPager.adapter = ViewPager2Adapter(this)
+
+
+        TabLayoutMediator(binding.tabLayout,binding.viewPager,object : TabLayoutMediator.TabConfigurationStrategy{
+            override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+                //TDDO HW настраиваем tab
+                tab.text = when(position){
+                    0-> "Earth"
+                    1-> "System"
+                    else -> "Mars"
+                }
+            }
+        } ).attach()
+    }
+}
---
+++ app/src/main/java/com/inflames1986/nasawithmaterial/view/navigation/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.inflames1986.nasawithmaterial.view.navigation.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.inflames1986.nasawithmaterial.view.navigation.EarthFragment
+import com.inflames1986.nasawithmaterial.view.navigation.MarsFragment
+import com.inflames1986.nasawithmaterial.view.navigation.SystemFragment
+
+class ViewPagerAdapter(private val fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+
+    private val fragments = arrayOf(EarthFragment(), SystemFragment(), MarsFragment())//TODO HW зафиксировать фрагменты на своих местах
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0-> "Earth"
+            1-> "System"
+            else -> "Mars"
+        }
+    }
+}
