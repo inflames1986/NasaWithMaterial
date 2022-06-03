f55a6f620558de77c2d93767ea3527e0787d3d8e
TMichael <js5859168@gmail.com>
2022-06-03 14:44:20 +0300

    Добавил фрагменты и лэйауты земля, марс, солн. система

---
+++ app/src/main/java/com/inflames1986/nasawithmaterial/view/navigation/EarthFragment.kt
@@ -0,0 +1,45 @@
+package com.inflames1986.nasawithmaterial.view.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.inflames1986.nasawithmaterial.R
+import com.inflames1986.nasawithmaterial.databinding.FragmentEarthBinding
+
+class EarthFragment : Fragment() {
+
+
+    private var _binding: FragmentEarthBinding? = null
+    private val binding: FragmentEarthBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentEarthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = EarthFragment()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/inflames1986/nasawithmaterial/view/navigation/MarsFragment.kt
@@ -0,0 +1,45 @@
+package com.inflames1986.nasawithmaterial.view.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.inflames1986.nasawithmaterial.R
+import com.inflames1986.nasawithmaterial.databinding.FragmentMarsBinding
+
+class MarsFragment : Fragment() {
+
+
+    private var _binding: FragmentMarsBinding? = null
+    private val binding: FragmentMarsBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMarsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MarsFragment()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
